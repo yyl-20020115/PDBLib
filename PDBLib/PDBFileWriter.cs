7ada808945a35fb6e0c51a89699df4b4137d4eb2
YILIN <YILIN@DESKTOP-YILIN>
2022-06-20 14:46:31 +0800

    apache license removed

---
+++ PDBLib/PDBFileWriter.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PDBLib
+{
+    public class PDBFileWriter
+    {
+        protected Stream stream;
+        public PDBFileWriter(Stream stream)
+        {
+            this.stream = stream;
+        }
+
+        public void Seek(long offset)
+        {
+            this.stream.Seek(offset, SeekOrigin.Begin);
+        }
+        public void Reserve(uint length)
+        {
+            this.Seek(this.stream.Position + length);
+        }
+        public void Write<T>(T t) where T : struct
+        {
+            this.Write(Utils.From(t)); 
+        }
+        public void Write(byte[] bytes)
+        {
+            this.stream.Write(bytes);
+        }
+        public void WriteByte(byte b)
+        {
+            this.stream.WriteByte(b);   
+        }
+    }
+}
--- PDBLib/PDBGenerator.cs
+++ PDBLib/PDBGenerator.cs
@@ -8,13 +8,26 @@ namespace PDBLib
 {
     public class PDBGenerator
     {
+        protected PDBFileWriter writer;
         public bool Load(PDBDocument doc)
         {
             return false;
         }
         public bool Generate(string path)
         {
+            using var stream = File.OpenWrite(path);
+            return this.Generate(stream);
+        }
+        public bool Generate(Stream stream)
+        {
+            this.writer = new PDBFileWriter(stream);
+
+
+
             return false;
         }
+
+
+
     }
 }
